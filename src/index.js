9ceff8208a502d9ebc6cfc2e31eaf485401cb88a
Teemo Tebest <teemo.tebest@gmail.com>
2023-03-22 16:37:38 +0100

    First demo

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 import { createRoot } from 'react-dom/client';
 
-import App from './jsx/App.jsx';
+import Figure1 from './jsx/Figure1.jsx';
+import Figure2 from './jsx/Figure2.jsx';
 
-const container = document.getElementById('app-root-2023-global_trade_trends');
-const root = createRoot(container);
-root.render(<App />);
+const containerFigure1 = document.getElementById('app-root-2023-global_trade_trends1');
+if (containerFigure1) {
+  const root = createRoot(containerFigure1);
+  root.render(<Figure1 />);
+}
+
+const containerFigure2 = document.getElementById('app-root-2023-global_trade_trends2');
+if (containerFigure2) {
+  const root = createRoot(containerFigure2);
+  root.render(<Figure2 />);
+}
